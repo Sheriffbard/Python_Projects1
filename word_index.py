0422581c9306c9ddc868d20a3897f45d4b76fdc1
Sheriffbard <olawuyiola@gmail.com>
2023-01-26 18:27:46 -0800

    longest_word_index

---
+++ word_index.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""a function called word_index that takes one argument, 
+
+a list of strings and returns the index of the longest word in the 
+
+list. If there is no longest word (if all the strings are of the same 
+
+length), the function should return zero (0)"""
+
+def word_index (list1):
+    for word in list1:
+    
+        for words in range(len(list1) - 1):
+    
+        
+            if len(list1[words]) == len(list1[words+ 1]):
+           
+                return 0
+            elif len(word) == len(max(list1)):
+            
+                return list1.index(word)
+
+words1 = ["Hate", "remorse", "vengeance"]
+words2 = ["Love", "Hate"]
+
+#call function
+
+print(word_index(words1))
+print(word_index(words2))
+
